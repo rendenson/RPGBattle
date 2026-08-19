0982000f8106e7d3fb938214e10ba705d692c853
Mykyta Zaitsev <mykyta.zaitsev@student.pk.edu.pl>
2026-08-19 20:15:14 +0200

    Add Rogue class with Agility stat

--- RPGBattle/Rogue.cs
+++ RPGBattle/Rogue.cs
@@ -1,6 +1,17 @@
 ﻿namespace RPGBattle;
 
-public class Rogue
+public class Rogue : Character
 {
+    // Constructor
+    public Rogue(string name, int maxHealth, int agility) : base(name, maxHealth)
+    {
+        if (agility <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(agility), "Agility must be greater than 0");
+        }
+        Agility = agility;
+    }
     
+    // Properties
+    public int Agility { get; }
 }

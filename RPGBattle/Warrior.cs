aafb653e1ed1c29efe915ec4e93b17012ad9bcfd
Mykyta Zaitsev <mykyta.zaitsev@student.pk.edu.pl>
2026-08-19 19:38:42 +0200

    Add Warrior class with AttackPower stat

---
+++ RPGBattle/Warrior.cs
@@ -0,0 +1,17 @@
+﻿namespace RPGBattle;
+
+public class Warrior : Character
+{
+    // Constructor
+    public Warrior(string name, int maxHealth, int attackPower) : base(name, maxHealth)
+    {
+        if (attackPower <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(attackPower), "Attack must be greater than 0.");
+        }
+        AttackPower = attackPower;
+    }
+    
+    // Properties
+    public int AttackPower { get; }
+}
